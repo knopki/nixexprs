64a1a2c86eb358add2b9ccd2d8739f8ab24da9d5
Sergey Korolev <korolev.srg@gmail.com>
2019-11-02 02:30:49 +0300

    fix: niv overlay

--- overlays/niv.nix
+++ overlays/niv.nix
@@ -2,5 +2,5 @@ self: super: {
   niv =
     if (super.lib.versionAtLeast super.haskellPackages.niv.version "0.2.1")
     then super.haskellPackages.niv
-    else (import ../nix/sources.nix).niv;
+    else (import (import ../nix/sources.nix).niv {}).niv;
 }
