2d54b74b3a2179f99af52c1adc9e85a9743374bf
Sergey Korolev <korolev.srg@gmail.com>
2020-01-19 00:50:21 +0300

    chore: remove some unneeded overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,11 +3,8 @@ let
     # Add your overlays here
     fishPlugins = import ./fishPlugins.nix;
     lsColors = import ./lsColors.nix;
-    morph = import ./morph.nix;
     neovim-gtk = import ./neovim-gtk.nix;
-    niv = import ./niv.nix;
     nixfromnpm = import ./nixfromnpm.nix;
-    nixpkgs-fmt = import ./nixpkgs-fmt.nix;
     nix-direnv = import ./nix-direnv.nix;
     nodePackages = import ./nodePackages.nix;
     pulumi = import ./pulumi.nix;
--- overlays/niv.nix
+++
@@ -1,6 +0,0 @@
-self: super: with super; {
-  niv =
-    if (lib.versionAtLeast haskellPackages.niv.version "0.2.1")
-    then haskellPackages.niv
-    else (import (import ../nix/sources.nix).niv {}).niv;
-}
