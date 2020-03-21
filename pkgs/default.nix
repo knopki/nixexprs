211e804c331afd3eb164b437983ec6051d6d3f96
Sergey Korolev <korolev.srg@gmail.com>
2020-03-21 21:30:15 +0300

    fix: build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> {}, lib, newScope, recurseIntoAttrs, sources ? import ../nix/sources.nix, nodejs ? pkgs.nodejs }:
 lib.makeScope newScope (
   self: with self; let
+    callPackage = lib.callPackageWith (pkgs // self // { inherit sources; });
     callPackages = lib.callPackagesWith (pkgs // self // { inherit sources; });
     mkNodeLib = import ./nodeLib {
       self = mkNodeLib;
@@ -11,14 +12,14 @@ lib.makeScope newScope (
   in
     rec {
       fishPlugins = recurseIntoAttrs (callPackages ./fish-plugins {});
-      lsColors = callPackages ./ls-colors.nix {};
-      neovim-gtk = callPackages ./neovim-gtk {};
-      nixfromnpm = callPackages ./nixfromnpm {};
-      kube-score = callPackages ./kube-score {};
-      kustomize1 = callPackages ./kustomize1 {};
-      nix-direnv = callPackages ./nix-direnv.nix {};
+      lsColors = callPackage ./ls-colors.nix {};
+      neovim-gtk = callPackage ./neovim-gtk {};
+      nixfromnpm = callPackage ./nixfromnpm {};
+      kube-score = callPackage ./kube-score {};
+      kustomize1 = callPackage ./kustomize1 {};
+      nix-direnv = callPackage ./nix-direnv.nix {};
       vimPlugins = recurseIntoAttrs (callPackages ./vimPlugins.nix {});
-      winbox-bin = callPackages ./winbox {};
+      winbox-bin = callPackage ./winbox {};
       winbox = winbox-bin;
     } // nodeLib.generatePackages {
       nodePackagesPath = ./nodePackages;
